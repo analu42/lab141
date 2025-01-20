c1a2a167fe19661e4d6a5785e61558580baefd12
analu <130846375+analu42@users.noreply.github.com>
2025-01-20 20:45:31 -0300

    .

--- lab141/sketch.js
+++ lab141/sketch.js
@@ -6,13 +6,15 @@ function preload(){
 
 function setup(){
     createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+    let canvas = createCanvas(400, 600); // Exemplo com dimensões de 400x400
+    canvas.parent('p5-container'); // Associa o canvas a um contêiner HTML
 }
 
 function draw(){
     camera(-500, -1000, 100, 0, 0, 0, 0, 0, -2);
     colorMode (HSL);
     background(0, 85.6, 53.7);
-    scale (width * 0.0014);
+    scale (width * 0.007);
     orbitControl(7, 7, 7);
     noStroke();
     directionalLight (0, 0, 500, 0.5, 0.5, -0.5);
