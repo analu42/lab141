b4b0d911ce77f41ec60478945cce5b6b0d3eabb7
analu <130846375+analu42@users.noreply.github.com>
2025-01-19 22:14:56 -0300

    ..

--- lab141/sketch.js
+++ lab141/sketch.js
@@ -9,13 +9,13 @@ function setup(){
 }
 
 function draw(){
-    //camera (0, 1000, 200);
-    colorMode (RGB);
-    background(214, 63, 63);
+    camera(-500, -1000, 100, 0, 0, 0, 0, 0, -2);
+    colorMode (HSL);
+    background(0, 85.6, 53.7);
     scale (width * 0.0014);
-    orbitControl(1, 1, 0);
+    orbitControl(7, 7, 7);
     noStroke();
-    directionalLight (0, 0, 840, 0.5, 0.5, -0.5);
+    directionalLight (0, 0, 500, 0.5, 0.5, -0.5);
     specularMaterial (250);
     shininess (1);
     model(ender);
